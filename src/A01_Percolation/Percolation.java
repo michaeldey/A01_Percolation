d01e2b26b5fee3497bd52d7ed83f446f21c11257
michaeldey <michaeldey@hotmail.com>
2017-01-27 15:36:00 -0700

    A01_Percolation
    master

---
+++ src/A01_Percolation/Percolation.java
@@ -0,0 +1,53 @@
+package A01_Percolation;
+
+public class Percolation {
+	
+	 public Percolation(int N){
+		 // create N‐by‐N grid, with all sites blocked 
+	 }
+		 
+	 public void open(int i, int j){          
+			 // open site (row i, column j) if it is not open already
+		 
+		 /*
+		  *  Throw a java.lang.IndexOutOfBoundsException 
+		  *  if any argument to open(), 
+		  *  isOpen(), or isFull() is outside its prescribed range
+		  */
+	 }
+			 
+	 public boolean isOpen(int i, int j){			 
+		 // is site (row i, column j) open?
+		 
+		 /*
+		  *  Throw a java.lang.IndexOutOfBoundsException 
+		  *  if any argument to open(), 
+		  *  isOpen(), or isFull() is outside its prescribed range
+		  */
+		 
+		 return false;
+	 }
+		 
+	 public boolean isFull(int i, int j){     
+		 // is site (row i, column j) full?
+		 
+		 /*
+		  *  Throw a java.lang.IndexOutOfBoundsException 
+		  *  if any argument to open(), 
+		  *  isOpen(), or isFull() is outside its prescribed range
+		  */
+		 
+		 return false;
+	 }
+		 
+	 public boolean percolates(){             
+		 // does the system percolate?
+		 return false;
+	 }
+
+	public String numberOfOpenSites() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/A01_Percolation/PercolationStats.java
@@ -0,0 +1,32 @@
+package A01_Percolation;
+
+public class PercolationStats {
+
+	public PercolationStats(int N, int T){  
+		// perform T independent experiments on an N‐by‐N grid
+	}
+	
+	public double mean(){                   
+		// sample mean of percolation threshold
+		
+		return 0.0;
+	}
+	
+	public double stddev(){                 
+		// sample standard deviation of percolation threshold  
+		return 0.0;
+	}
+	
+	public double confidenceLow(){          
+		// low  endpoint of 95% confidence interval
+		
+		return 0.0;
+	}
+	
+	public double confidenceHigh(){         
+		// high endpoint of 95% confidence interval
+		
+		return 0.0;
+	}
+	
+}
