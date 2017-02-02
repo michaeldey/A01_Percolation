cc438e302281bbe78144fd65c345f96b78d44f4a
michaeldey <michaeldey@hotmail.com>
2017-02-01 18:20:07 -0700

    calculate mean and standard deviation

--- src/A01_Percolation/PercolationStats.java
+++ src/A01_Percolation/PercolationStats.java
@@ -1,6 +1,7 @@
 package A01_Percolation;
 
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 	
@@ -16,7 +17,7 @@ public class PercolationStats {
 	private int T; // T is the number of experiments to run	
 	private int[] nArray; //holds the values to feed to Percolation, values will be shuffled
 	private Percolation perc;
- 
+	private int[] percScores;// array to record all the percolation scores
 
 	public PercolationStats(int N, int T){
 		//The constructor should take two arguments N and T		
@@ -28,45 +29,55 @@ public class PercolationStats {
 		this.T= T;
 		
 		int nByN = N * N;// speed up the process by not constantly multiplying N x N
-
+		int count = 0;
+		
+		percScores = new int[T]; // array to record all the percolation scores
+		int pSPointer = 0; //pointer for percScores[]
+		
 		
 		//create an array of all possible input values
 		nArray = new int[nByN];
 		for (int i = 0; i < (nByN); i++){nArray[i] = i;}
 		
 		// perform T independent experiments on an N‐by‐N grid
-		while (T >= 0){
+		while (T > 0){
 			perc = new Percolation(N); //create a new Percolation object of N width
 			shuffle(nArray); //shuffle the array so it contains random numbers
 			
 			for (int m : nArray){
 				perc.open(getX(m), getY(m));
-				if (perc.percolates()) break; //stop once percolation occurs
+				count++;
+				if (perc.percolates()){
+					System.out.println("Percolates at: " + count);
+					percScores[pSPointer] = count;//record count into scores array
+					pSPointer++;//increment the pointer one
+					count = 0; //reset the count
+					break; //stop once percolation occurs and go to next simulation
+				}
 			}
 		
 
 			T--;
 		}
+		
+		//print out the values of percScores
+		System.out.println("Perc Scores:");
+		for (int m : percScores){
+			System.out.printf("%d\n", m);
+		}
+		
+		System.out.printf("Mean: %f\n", mean());
+		System.out.printf("Standard Deviation: %f\n", stddev());
 	}//end of constructor
 	
 
 
 	public double mean(){                   
-		// sample mean of percolation threshold
-		
-		//use standard statistics from stdlib.jar 
-		//to compute the sample mean and standard deviation
-		
-		return 0.0;
+		return StdStats.mean(percScores);
 	}
 	
 	public double stddev(){                 
-		// sample standard deviation of percolation threshold
-		
-		//use standard statistics from stdlib.jar 
-		//to compute the sample mean and standard deviation
-		
-		return 0.0;
+		return StdStats.stddev(percScores);
 	}
 	
 	public double confidenceLow(){          
